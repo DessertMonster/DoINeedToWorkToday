83bcb1163affdc236c6352c13ebe2738356f6182
Winnie Ho <winnie.ho@devfacto.com>
2019-02-02 13:28:18 -0700

    Migrate from .NET Core 2.0 to 2.1

--- src/DoINeedToWorkToday.Api/Startup.cs
+++ src/DoINeedToWorkToday.Api/Startup.cs
@@ -1,13 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Builder;
+﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Logging;
-using Microsoft.Extensions.Options;
 using Swashbuckle.AspNetCore.Swagger;
 
 namespace DoINeedToWork.Api
@@ -24,7 +19,8 @@ namespace DoINeedToWork.Api
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddMvc();
+            services.AddMvc()
+                .SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
 
             services.AddSwaggerGen(c => {
                 c.SwaggerDoc("v1", new Info {
@@ -46,6 +42,7 @@ namespace DoINeedToWork.Api
             app.UseSwaggerUI(c => {
                 c.SwaggerEndpoint("/swagger/v1/swagger.json", "Do I Need To Work API V1");
             });
+            app.UseHttpsRedirection();
             app.UseMvc();
         }
     }
