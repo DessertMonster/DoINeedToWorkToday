9ef62a602acbde91701bc06f7ce7c7fb50da7067
Winnie Ho <winnie.ho@devfacto.com>
2019-03-17 01:27:48 -0600

    Notify pipeline of successful lambda invocation

--- src/DoINeedToWork.Lambda/Function.cs
+++ src/DoINeedToWork.Lambda/Function.cs
@@ -1,4 +1,8 @@
+using Amazon;
+using Amazon.CodePipeline;
 using Amazon.Lambda.Core;
+using System.Linq;
+using System.Threading.Tasks;
 
 // Assembly attribute to enable the Lambda function's JSON input to be converted into a .NET class.
 [assembly: LambdaSerializer(typeof(Amazon.Lambda.Serialization.Json.JsonSerializer))]
@@ -7,16 +11,33 @@ namespace DoINeedToWork.Lambda
 {
     public class Function
     {
-        
         /// <summary>
         /// A simple function that takes a string and does a ToUpper
         /// </summary>
         /// <param name="input"></param>
         /// <param name="context"></param>
         /// <returns></returns>
-        public void FunctionHandler(ILambdaContext context)
+        public async Task FunctionHandler(ILambdaContext context)
         {
             LambdaLogger.Log($"{context.FunctionName} was invoked as a CodePipeline stage.");
+
+            var client = new AmazonCodePipelineClient(RegionEndpoint.USWest2);
+
+            var poll = await client.PollForJobsAsync(new Amazon.CodePipeline.Model.PollForJobsRequest {
+                ActionTypeId = new Amazon.CodePipeline.Model.ActionTypeId
+                {
+                    Category = ActionCategory.Invoke,
+                    Provider = "Lambda"
+                }
+            });
+
+            var jobId = poll.Jobs.First().Id;
+
+            LambdaLogger.Log($"Job id is {jobId}");
+
+            await client.PutJobSuccessResultAsync(new Amazon.CodePipeline.Model.PutJobSuccessResultRequest {
+                JobId = jobId
+            });
         }
     }
 }
